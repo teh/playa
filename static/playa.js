d144a62a846bfa4bd88a53c05afbf8e53409d070
Thomas Hunger <tehunger@gmail.com>
2013-04-17 23:38:51 +0100

    Implement skip-to-next-song after end.

--- static/playa.js
+++ static/playa.js
@@ -1,9 +1,23 @@
 app = angular.module('playa', [])
 
+app.directive('audioControls', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            angular.element(element).bind('progress', function() {
+            })
+            angular.element(element).bind('ended', function() {
+                scope.onNext()
+                scope.$digest()
+            })
+        }
+    }
+})
 
 var PlaylistController = function($scope, $http) {
     $scope.src = null
     $scope.currentTitle = "No title"
+    $scope.playlist = []
     $http.get('/music').success(function(data) {
         $scope.playlist = data.songs
     })
@@ -11,9 +25,19 @@ var PlaylistController = function($scope, $http) {
     $scope.onPlay = function(path, title) {
         $scope.src = path
         $scope.currentTitle = title
-        console.log(document.getElementById("audio"))
         setTimeout(function() {
             document.getElementById("audio").play()
         }, 5)
+    }
+
+    $scope.onNext = function() {
+        for (var i = 0; i < $scope.playlist.length; i++) {
+            var e = $scope.playlist[i]
+            if (e.title == $scope.currentTitle) {
+                e = $scope.playlist[i + 1]
+                $scope.onPlay(e.path, e.title)
+                return
+            }
+        }
     }
 }
